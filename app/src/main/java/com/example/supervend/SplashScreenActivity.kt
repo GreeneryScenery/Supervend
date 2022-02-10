c5264fb46d4593c6ec344eaf7f9ff5ec30b5e0e7
Moonkake <yuebin0303@gmail.com>
2022-02-10 14:04:50 +0800

    splash screen changes 1

--- app/src/main/java/com/example/supervend/SplashScreenActivity.kt
+++ app/src/main/java/com/example/supervend/SplashScreenActivity.kt
@@ -2,6 +2,6 @@ package com.example.supervend
 
 import androidx.appcompat.app.AppCompatActivity
 
-class SplashScreen : AppCompatActivity() {
+class SplashScreenActivity : AppCompatActivity() {
 
 }
