9f013a75860d0fbabb4e4720f9faf27d65adc4ef
GreeneryScenery <89194387+GreeneryScenery@users.noreply.github.com>
2022-02-10 20:47:22 +0800

    Keyboard stuff

--- app/src/main/java/com/example/supervend/SignUp.kt
+++ app/src/main/java/com/example/supervend/SignUp.kt
@@ -1,7 +1,11 @@
 package com.example.supervend
 
 import android.os.Bundle
+import android.view.MotionEvent
+import android.view.View
+import android.view.inputmethod.InputMethodManager
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
@@ -92,5 +96,23 @@ class SignUp : AppCompatActivity() {
                 alertDialog.show()
             }
         }
+    }
+
+    override fun dispatchTouchEvent(ev: MotionEvent): Boolean {
+        val view: View? = currentFocus
+        if (view != null && (ev.action == MotionEvent.ACTION_UP || ev.action == MotionEvent.ACTION_MOVE) && view is EditText && !view.javaClass
+                .getName().startsWith("android.webkit.")
+        ) {
+            val scrcoords = IntArray(2)
+            view.getLocationOnScreen(scrcoords)
+            val x: Float = ev.rawX + view.getLeft() - scrcoords[0]
+            val y: Float = ev.rawY + view.getTop() - scrcoords[1]
+            if (x < view.getLeft() || x > view.getRight() || y < view.getTop() || y > view.getBottom()) (this.getSystemService(
+                INPUT_METHOD_SERVICE
+            ) as InputMethodManager).hideSoftInputFromWindow(
+                this.window.decorView.applicationWindowToken, 0
+            )
+        }
+        return super.dispatchTouchEvent(ev)
     }
 }
