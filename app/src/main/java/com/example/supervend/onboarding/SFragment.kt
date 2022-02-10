92227f0c0d33ee5b7e9995340934e82d1bb9a990
GreeneryScenery <89194387+GreeneryScenery@users.noreply.github.com>
2022-02-10 17:58:43 +0800

    Onboarding stuff

---
+++ app/src/main/java/com/example/supervend/onboarding/SFragment.kt
@@ -0,0 +1,15 @@
+package com.example.lab4.onboarding
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.supervend.R
+
+class SFragment : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.third_fragment, container, false)
+    }
+}
