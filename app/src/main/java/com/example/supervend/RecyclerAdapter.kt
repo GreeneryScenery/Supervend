0f2b4b769990de7570bb3ce84072cbcde6a78b38
Moonkake <yuebin0303@gmail.com>
2022-02-10 18:33:33 +0800

    added item activity (not done)

--- app/src/main/java/com/example/supervend/RecyclerAdapter.kt
+++ app/src/main/java/com/example/supervend/RecyclerAdapter.kt
@@ -1,31 +1,40 @@
 package com.example.recyclerviewcardview
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 import android.widget.TextView
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.view.LayoutInflater
+import androidx.core.content.ContextCompat
+import androidx.core.graphics.drawable.toBitmap
 import com.example.supervend.Item
+import com.example.supervend.ItemActivity
 import com.example.supervend.R
-import com.google.android.material.snackbar.Snackbar
 
 class RecyclerAdapter(val itemList: ArrayList<Item>) : RecyclerView.Adapter<RecyclerAdapter.ViewHolder>() {
+
+    private lateinit var descArray : Array<String>
+    private lateinit var brandArray : Array<String>
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val v : View = LayoutInflater.from(parent.context)
             .inflate(R.layout.card_layout,parent,false)
-        return ViewHolder(v)
+
+        descArray = v.resources.getStringArray(R.array.item_descriptions)
+        brandArray = v.resources.getStringArray(R.array.item_brands)
+        return ViewHolder(v, descArray, brandArray)
 
     }
-    override fun onBindViewHolder(holder: RecyclerAdapter.ViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         holder.bindItems(itemList[position])
     }
     override fun getItemCount() = itemList.size
 
     // The class holding the list view
-    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    class ViewHolder(itemView: View, descArray: Array<String>, brandArray: Array<String>) : RecyclerView.ViewHolder(itemView) {
         var itemImage: ImageView = itemView.findViewById(R.id.item_image)
         var itemTitle: TextView = itemView.findViewById(R.id.item_title)
         var itemPrice: TextView = itemView.findViewById(R.id.item_price)
@@ -33,9 +42,15 @@ class RecyclerAdapter(val itemList: ArrayList<Item>) : RecyclerView.Adapter<Recy
 
         init {
             itemView.setOnClickListener{ view ->
-                val pos = adapterPosition +1
-                Snackbar.make(view, "Click detected on item $pos", Snackbar.LENGTH_LONG)
-                    .setAction("Action",null).show()
+                val intent = Intent(view.context, ItemActivity::class.java)
+                intent.putExtra("image", itemImage.drawable.toBitmap())
+                intent.putExtra("title", itemTitle.text)
+                intent.putExtra("price", itemPrice.text)
+                intent.putExtra("weight", itemWeight.text)
+                intent.putExtra("brand", brandArray[adapterPosition])
+                intent.putExtra("description", descArray[adapterPosition])
+                ContextCompat.startActivity(view.context, intent, null)
+
             }
         }
 
