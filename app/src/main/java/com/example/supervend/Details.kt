0a66e7a8d1f917400f47bef5cfb21814b9b9efbb
GreeneryScenery <89194387+GreeneryScenery@users.noreply.github.com>
2022-02-11 19:50:53 +0800

    Details stuff

--- app/src/main/java/com/example/supervend/Details.kt
+++ app/src/main/java/com/example/supervend/Details.kt
@@ -24,7 +24,7 @@ class Details(val itemList: ArrayList<Item>) : RecyclerView.Adapter<Details.View
         var textView : TextView = itemView.findViewById(R.id.detailsView)
 
         fun bindItems(item : Item){
-            textView.text = "Name: ${item.name}\nPrice: $${item.price}\nWeight: ${item.weight}g\nBrand: ${item.brand}\nDetails: ${item.description}"
+            textView.text = "Name: ${item.name}\nPrice: ${"$%.2f".format(item.price)}\nWeight: ${item.weight}g\nBrand: ${item.brand}\nDetails: ${item.description}"
         }
     }
 }
