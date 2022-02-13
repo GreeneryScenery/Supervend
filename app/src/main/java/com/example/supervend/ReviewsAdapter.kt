eeb4466a95d5a712927eedbf3109e0f70e58bad5
Moonkake <yuebin0303@gmail.com>
2022-02-13 14:42:32 +0800

    listadapter finished i think

--- app/src/main/java/com/example/supervend/ReviewsAdapter.kt
+++ app/src/main/java/com/example/supervend/ReviewsAdapter.kt
@@ -1,30 +1,34 @@
-import android.R
 import android.content.Context
+import android.media.Rating
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.ImageView
+import android.widget.RatingBar
 import android.widget.TextView
+import com.example.supervend.R
 import com.example.supervend.Review
+import de.hdodenhof.circleimageview.CircleImageView
 import java.util.ArrayList
 
-class ListAdapter(context: Context, val layoutResource: Int, userArrayList: ArrayList<Review?>) :
-    ArrayAdapter<Review?>(context, layoutResource, userArrayList) {
+class ListAdapter(context: Context, userArrayList: ArrayList<Review?>) :
+    ArrayAdapter<Review?>(context, R.layout.list_review_item, userArrayList) {
     override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
-        var convertView = convertView
         val review: Review? = getItem(position)
         if (convertView == null) {
-            convertView = LayoutInflater.from(context).inflate(layoutResource, parent, false)
+            convertView = LayoutInflater.from(context).inflate(R.layout.list_review_item, parent, false)
         }
-        val imageView = convertView!!.findViewById<ImageView>(R.id.profile_pic)
-        val userName = convertView.findViewById<TextView>(R.id.personName)
-        val lastMsg = convertView.findViewById<TextView>(R.id.lastMessage)
-        val time = convertView.findViewById<TextView>(R.id.msgtime)
-        imageView.setImageResource(user.imageId)
-        userName.setText(user.name)
-        lastMsg.setText(user.lastMessage)
-        time.setText(user.lastMsgTime)
+        val imageView = convertView!!.findViewById<CircleImageView>(R.id.profile_pic)
+        val userName = convertView.findViewById<TextView>(R.id.nameTXT)
+        val ratingBar = convertView.findViewById<RatingBar>(R.id.reviewRatingBar)
+        val reviewMsg = convertView.findViewById<TextView>(R.id.reviewTXT)
+        if (review != null) {
+            imageView.setImageResource(review.image)
+            userName.text = review.name
+            ratingBar.rating = review.rating
+            reviewMsg.text = review.review
+        }
         return convertView
     }
 }
