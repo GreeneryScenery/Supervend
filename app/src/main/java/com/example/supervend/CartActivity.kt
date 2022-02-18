a10b87c38ee828466e4cdbe2b66eb08bb4e90a7b
GreeneryScenery <89194387+GreeneryScenery@users.noreply.github.com>
2022-02-18 16:26:01 +0800

    Cart stuff

--- app/src/main/java/com/example/supervend/CartActivity.kt
+++ app/src/main/java/com/example/supervend/CartActivity.kt
@@ -1,6 +1,18 @@
 package com.example.supervend
 
+import android.os.Bundle
+import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
 
 class CartActivity  : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN)
+
+        setContentView(R.layout.activity_cart)
+    }
 }
