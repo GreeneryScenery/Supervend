c5767157ddf0c503eba55e7f41aab755d09e0d07
Moonkake <yuebin0303@gmail.com>
2022-02-18 22:50:12 +0800

    sheesh

--- app/src/main/java/com/example/supervend/CartAdapter.kt
+++ app/src/main/java/com/example/supervend/CartAdapter.kt
@@ -1,5 +1,6 @@
 package com.example.supervend
 
+import android.annotation.SuppressLint
 import android.opengl.Visibility
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
@@ -58,9 +59,10 @@ class CartAdapter(private val cartList: ArrayList<CartItem>) : RecyclerView.Adap
             }
         }
 
+        @SuppressLint("SetTextI18n")
         fun bindItems(cartItem: CartItem){
             cartName.text = cartItem.name
-            cartAmount.text = cartItem.amount.toString()
+            cartAmount.text = "Amount: ${cartItem.amount}"
             cartItem.image?.let { cartImage.setImageResource(it) }
         }
     }
