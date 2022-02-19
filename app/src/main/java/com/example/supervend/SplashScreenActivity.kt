d8166b3bcc109b0bc45003a292cd894cf94283dc
GreeneryScenery <89194387+GreeneryScenery@users.noreply.github.com>
2022-02-19 20:06:31 +0800

    Mode stuff

--- app/src/main/java/com/example/supervend/SplashScreenActivity.kt
+++ app/src/main/java/com/example/supervend/SplashScreenActivity.kt
@@ -2,6 +2,7 @@ package com.example.supervend
 
 import android.annotation.SuppressLint
 import android.content.Intent
+import android.content.res.Configuration
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
@@ -9,6 +10,7 @@ import android.view.WindowManager
 import android.view.animation.AnimationUtils
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.core.os.HandlerCompat.postDelayed
 import pl.droidsonroids.gif.GifImageView
 
@@ -21,6 +23,21 @@ class SplashScreenActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
         setContentView(R.layout.splash_screen)
+
+        val sp = getSharedPreferences("settings", MODE_PRIVATE)
+
+        if (sp.getBoolean("darkMode",
+                this.resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK == Configuration.UI_MODE_NIGHT_YES)){
+            val myEdit = sp.edit()
+            myEdit.putBoolean("darkMode", true)
+            myEdit.apply()
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+        } else{
+            val myEdit = sp.edit()
+            myEdit.putBoolean("darkMode", false)
+            myEdit.apply()
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+        }
 
         // Animations
         val topAnim = AnimationUtils.loadAnimation(this, R.anim.top_animation)
